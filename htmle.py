a6f60bb15793dddba11d40ff98884c3ec89227ae
Dave Levy <davelevy.eu@gmail.com>
2020-07-20 14:15:39 +0100

    htmle.tr
    This program defines a function (tr) that translates its known symbols into the corresponding HTML codes.

---
+++ htmle.py
@@ -0,0 +1,23 @@
+
+# DFL 20 Jul 2020
+
+# this program defines a function that translates its known symbols into the
+# corresponding HTML codes. 
+
+
+htmlcodes={ "'" : '&apos;',     '£':'&pound;',  '& ':'&amp;',    '€': '&euro;',
+            "ß": '&szlig;',     'ä': '&auml;',  'ö':'&ouml;',   'ü': '&uuml;',
+            'Ä': '&Auml;',      'Ö': '&Oml;',   'Ü': 'Uuml;',   'µ': '&micro;',
+           '-': '&#45;',        '/': '&#47;'}
+
+
+# this function takes two parameters, the text to be converted and a dictionary
+# holding a series of (symbol: html code) pairs, it will use mine dictionary as
+# a default
+
+def tr (txt,codes=htmlcodes):
+    for sym in codes.keys():
+        txt=txt.replace(sym,codes[sym])
+    o=txt
+    return txt
+
