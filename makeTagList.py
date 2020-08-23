1091c639c9ad3dcee5225dc2eef6d72c8aad9e8c
Dave Levy <davelevy.eu@gmail.com>
2020-08-23 21:57:37 +0100

    V2: makes a dict holding the tag and element keys

--- makeTagList.py
+++ makeTagList.py
@@ -7,36 +7,40 @@ dictfile = open('bookmarks.pickle','rb')
 
 D = {};                     # the input dictionary
 D = pickle.load(dictfile) ; # loads a dictionary
+dictfile.close()
+
+del D[3251]
+del D[3382]
+del D[3477]
+
 K=[]; K = D.keys();         # the input dictionary keys
 T = {} ;                    # the output dictionary, T for Tags
 
-#for e in [1,2,3,4]:
-for e in K:                 # somehow this doesn't work
-    #print (e, D[e]['TAGS']); # writes the keyed tag list
-    if ( int(D[e]['TAGS'].find(',')) > 0 ):  # if no commas, we have an empty tag iist
-        words = D[e]['TAGS'].split(',')
-        for w in words:
-            if not ( w == [''] ):
-                #print (e, w); # writes the inverted list index
-                if w in T:          # to see if key w is present in dict T
-                # print w, ' found at element ', e
-                    T[w] = T[w] + [e]
+for e in K:                 # for all keys in K
+    if D[e]['STATUS'] == 'OK':
+        #print (e, D[e]['TAGS']); # writes the keyed tag list
+        if ( int(D[e]['TAGS'].find(',')) > 0 ):  # if no commas, we have an empty tag iist
+            words = D[e]['TAGS'].split(',')
+            for w in words:
+                if not ( w == [''] ):
+                    if w in T:     # to see if key w is present in dict T
+                        T[w] = T[w] + [e]
+                    else:
+                        T[w]=[e]
                 else:
-                # print w, ' NOT found at element ', e
-                    T[w]=[e]
-            else:
-                print ('Null tag found')
+                    print ('Null tag found')
+
+# dict T holds the tags and their associated keys from K
+
+for t in T.keys():
+    print (t,T[t])
+
+# i could save this to another pickle db or write out the RSS files I want
+
+#
+
   
-print ('word=forum', T.get('forum'))
 
-#for each word in T, make a list holding numbers and anchor text    
-#print (T.keys())
-
-# print T
-
-# print (D.keys())
-#print D[1]
-#print D[2]
 
         
 
