3eebb029eb60f5e21e0e3644689761063c51daed
Dave Levy <davelevy.eu@gmail.com>
2020-08-24 11:45:58 +0100

    workround for ; error
    I have deleted the semi-colon entry for the moment as it was erroring by converting the code terminator ; to an HTML code

--- htmle.py
+++ htmle.py
@@ -5,16 +5,28 @@
 # corresponding HTML codes. 
 
 
-htmlcodes={ "'" : '&apos;', '£':'&pound;',  '& ':'&amp;',   '€': '&euro;',
+htmlcodes={ "'" : '&apos;', '£':'&pound;',  '& ':'&amp; ',  '€': '&euro;',
             'ß': '&szlig;', 'ä': '&auml;',  'ö':'&ouml;',   'ü': '&uuml;',
             'Ä': '&Auml;',  'Ö': '&Oml;',   'Ü': 'Uuml;',   'µ': '&micro;',
-            '-': '&#45;',   '/': '&#47;',   '’': '&apos;',  '!': '&#33;'
-            '"': '&quot;',  '#': '&#35;',   '$'; '&#36;',   '%': '&#37;',
-            '(': '&#40;',   ')': '&#41;' } 
+            '-': '&#45;',   '/': '&#47;',   '’': '&apos;',  '!': '&#33;',
+            '"': '&#34;',  '$': '&#36;',   '%': '&#37;',
+            '(': '&#40;',   ')': '&#41;',   '*': '&#42;',   '+': '&#43;',
+            ':': '&#58;',   '=': '&#61;',   '?': '&#63;',
+            '@': '&#64;' }
 
+# [ 	&#91; 	  	left square bracket
+# \ 	&#92; 	  	backslash
+# ] 	&#93; 	  	right square bracket
+# ^ 	&#94; 	  	caret / circumflex
+# _ 	&#95; 	  	underscore
+# ` 	&#96; 	  	grave / accent
+
+# I took ';': '&#59;', out as it was double printing 
+
+#Are we going to do ' #': ' &#35;'? And what about <>?
 
 # this function takes two parameters, the text to be converted and a dictionary
-# holding a series of (symbol: html code) pairs, it will use mine dictionary as
+# holding a series of (symbol: html code) pairs, it will use my dictionary as
 # a default
 
 def tr (txt,codes=htmlcodes):
